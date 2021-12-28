afc9b4688600c9c82db878c2c4cbca54adfbc730
Tomilov Sergey <tomexpert@yandex.ru>
2021-12-28 13:40:26 +0800

    rxjs v1.1

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import MailDesk from './maildesk';
 
 const domElmt = document.querySelector('.desk');
 
-// const servetPath = 'https://netology-ahj-http-heroku.herokuapp.com';
-const servetPath = 'http://localhost:7070';
+const servetPath = 'https://netology-ahj-http-heroku.herokuapp.com';
+// const servetPath = 'http://localhost:7070';
 const taskName = '/ahj-rxjs';
 
 const mDesk = new MailDesk(domElmt, `${servetPath}${taskName}`, 5000);
